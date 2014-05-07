7b8e7f76088ce784e51b9e3ff0db303600811008
ayush <ayushtiwariiitr@gmail.com>
2014-05-07 15:41:06 +0530

    Database problem solved

--- product_search/settings.py
+++ product_search/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ayush',                      # Or path to database file if using sqlite3.
+        'NAME': 'product',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'ayush',
         'PASSWORD': '24121993',
--- search/views.py
+++ search/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import render,render_to_response
 from django.template import RequestContext
 from django.core.context_processors import csrf
 from cra import *
+from search.forms import *
 
 def index(request):
   '''
     This view takes the entry url and use it in the script to get values and then displays the data , i.e, p here
   '''
-
-  if 'details' in request.POST:
-    details = request.POST['details']
-    p=parse(str(details))
-    print p
-    import pdb; pdb.set_trace;
-    return HttpResponse(p['image'])
-#    return render_to_response('search/index.html', p , context_instance=RequestContext(request))
+  form = url_form()
+  if request.method == 'POST':
+    form = url_form(request.POST)
+    if form.is_valid(): 
+      details =form.cleaned_data['content']
+      p=parse(details)
+    return render_to_response('search/result.html',{'form':form,
+                     'p':p  }, context_instance=RequestContext(request))
+  
   else:
-    return render(request, 'search/index.html')
+    form = url_form()
+    return render_to_response('search/index.html',{'form':form}, context_instance=RequestContext(request))
