8100a96ac5dd74750eb833890cb6eb2f9602b0ea
ayush <ayushtiwariiitr@gmail.com>
2014-05-08 12:44:29 +0530

    Defined extract function

---
+++ search/extract.py
@@ -0,0 +1,25 @@
+import csv
+from urlparse import urlparse
+from webscraping import download , xpath
+
+def extract(url):
+
+  D = download.Download()
+
+  file = open('website.csv', 'rb')
+  reader = csv.reader(file)
+  row = list(reader)
+
+  for r in range(0,3):
+    if url.find(row[r][0])>=0:
+      xpath1 = row[r][1]
+      xpath2 = row[r][2]
+      xpath3 = row[r][3]  
+
+      html = D.get(url)
+      item={}
+      item['name'] = xpath.get(html,'%s//text()' % xpath1)
+      item['price'] = xpath.get(html,'%s//text()' % xpath2)
+      item['image'] = xpath.get(html, '%s' % xpath3)
+
+      print item
