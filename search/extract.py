6d5130f4a589e5150506a97d68c20089ee379889
ayush <ayushtiwariiitr@gmail.com>
2014-05-08 17:32:54 +0530

    Errors check included and comments updated

--- search/extract.py
+++ search/extract.py
@@ -10,10 +10,10 @@ def extract(url):
   '''
   D = download.Download()
 
-  f = open(os.path.join(os.path.dirname(__file__),'website.csv'), 'rb')  #Joining absolute path so that the function can be used inside an app
+  f = open(os.path.join(os.path.dirname(__file__),'webpage_xpath.csv'), 'rb')  #Joining absolute path so that the function can be used inside an app
   reader = csv.reader(f)
   row = list(reader)
-  
+  item ={}
   for r in range(0,3):
     if url.find(row[r][0])>=0:
       xpath1 = row[r][1]
@@ -21,12 +21,18 @@ def extract(url):
       xpath3 = row[r][3]  
 
       html = D.get(url)
-      item={}   
+      
       item['name'] = xpath.get(html,'%s//text()' % xpath1)
       item['price'] = xpath.get(html,'%s//text()' % xpath2)
       item['image'] = xpath.get(html, '%s' % xpath3)
 
       return item
+
+    else:
+      continue
+  if item == {}: 
+    item={'name':'invalid url'}
+    return item
 
 if __name__ == '__main__':
    url="".join( sys.argv[1:] )
--- search/parse.py
+++
@@ -1,6 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read("site.ini")
-
-print config.sections()
